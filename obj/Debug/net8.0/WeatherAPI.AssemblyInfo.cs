3992d4750216834dc07681eff3370b9724574f42
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-01-20 19:14:01 +0100

    Quitar una carpeta que sobra

--- obj/Debug/net8.0/WeatherAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/WeatherAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3cb7ccecfd9b8f3b656de0d7df89d7eb0b11855")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f0425f137850b7b707c64706b827377e0296d0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
